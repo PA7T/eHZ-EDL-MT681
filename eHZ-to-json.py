b5ac238670c8f4144eecce0d8506f2657294145d
DL2ZZ / PA7T <PA7T@users.noreply.github.com>
2020-05-12 22:26:26 +0200

    changed to running the while loop inside python
    to reduce CPU load when running python3 every 2sec.

--- eHZ-to-json.py
+++ eHZ-to-json.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 
 import json
+import os
+
 
 logfile = "/dev/shm/eHZ-EDL-MT681.log"
 
-f = open(logfile,'r')
+while True:
 
-d = {} # create python dictionary
+    os.system("/home/dietpi/bin/sml_server -s /dev/ttyUSB0 > /dev/shm/eHZ-EDL-MT681.log");
 
-for line in f:
-    obis = line.strip().split('#')
-    d[obis[0]] = (obis[1],obis[2])
+    f = open(logfile,'r')
+
+    d = {} # create python dictionary
+
+    for line in f:
+        obis = line.strip().split('#')
+        d[obis[0]] = (obis[1],obis[2])
 #    print(line,end='')
 #    print(line.strip().split('#'))
 
-print(json.dumps(d,ensure_ascii=False))
+#    print(json.dumps(d,ensure_ascii=False))
+
+
+    with open("/dev/shm/eHZ-EDL-MT681.json",'w') as fileout:
+        fileout.write(json.dumps(d,ensure_ascii=False))
+
