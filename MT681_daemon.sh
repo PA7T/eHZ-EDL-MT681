97a7ea95a11883a283957628931eb32a992c134a
DL2ZZ / PA7T <PA7T@users.noreply.github.com>
2021-02-14 13:21:52 +0100

    Update MT681_daemon.sh
    updated from my device

--- MT681_daemon.sh
+++ MT681_daemon.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-while true
-do
-
-  /home/dietpi/bin/sml_server -s /dev/ttyUSB0 > /dev/shm/eHZ-EDL-MT681.log
-  python3 /home/dietpi/bin/eHZ-to-json.py > /dev/shm/eHZ-EDL-MT681.json
-
-done
+python3 /home/dietpi/bin/eHZ-to-json-pub-mqtt.py 
